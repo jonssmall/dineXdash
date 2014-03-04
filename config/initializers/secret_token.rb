9d148eab281cca3400a938c600fc8cd04c0d7f7a
Anika Tabassum <anika01@gmail.com>
2014-03-03 19:18:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DineXdash::Application.config.secret_key_base = '6a06a13c72c358b99e4f6b9ef59bdab2dfe764a02059a61ad81b0d8afe8a2af40de22f899147850467eb7c615a4d22db9290b79c6ade8a3e677141e711b65b05'
