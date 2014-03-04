f24cfe8435a27abb41a0594337f62a090615129d
Anika Tabassum <anika01@gmail.com>
2014-03-04 14:00:20 -0500

    Minor commit, migration and cleaned up restaurants controller comments a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304002439) do
+ActiveRecord::Schema.define(version: 20140304005859) do
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",            null: false
