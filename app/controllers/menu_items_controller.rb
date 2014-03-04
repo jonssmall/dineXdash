163f683e3313f5b5631fadae1e5437b561405ed8
Anika Tabassum <anika01@gmail.com>
2014-03-04 16:23:36 -0500

    Menu_items working nested with restaurants, need to fix redundant flash messages later

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,57 +1,61 @@
 class MenuItemsController < ApplicationController
   #before_action :set_menu_item, only: [:show, :edit, :update, :destroy]
+  before_filter :load_restaurant
 
   def index
-    @menu_items = MenuItem.all
+    @menu_items = @restaurant.menu_items.all
   end
 
   def show
+    @menu_item=@restaurant.menu_items.find(params[:id])
   end
 
   def new
-    @menu_item = MenuItem.new
+    @menu_item = @restaurant.menu_items.new
   end
 
   def edit
+    @menu_item = @restaurant.menu_items.find(params[:id])
   end
 
   def create
-    @menu_item = MenuItem.new(menu_item_params)
+    @menu_item = @restaurant.menu_items.new(menu_item_params)
 
     respond_to do |format|
       if @menu_item.save
-        format.html { redirect_to @menu_item, notice: 'Menu item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @menu_item }
+        format.html { redirect_to [@restaurant, @menu_item], notice: 'Menu item was successfully created.' }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @menu_item.errors, status: :unprocessable_entity }
+        format.html { render action: 'new' }  
       end
     end
   end
 
   def update
+    @menu_item=@restaurant.menu_items.find(params[:id])
     respond_to do |format|
       if @menu_item.update(menu_item_params)
-        format.html { redirect_to @menu_item, notice: 'Menu item was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to [@restaurant, @menu_item], notice: 'Menu item was successfully updated.' }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @menu_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
+    @menu_item = @restaurant.menu_items.find(params[:id])
     @menu_item.destroy
-    end
   end
 
   private
-    def set_menu_item
-      @menu_item = MenuItem.find(params[:id])
-    end
+    # def set_menu_item
+    #   @menu_item = MenuItem.find(params[:id])
+    # end
 
     def menu_item_params
       params.require(:menu_item).permit(:restaurant_id, :item_name, :item_desc, :price)
+    end
+
+    def load_restaurant
+      @restaurant=Restaurant.find(params[:restaurant_id])
     end
 end
