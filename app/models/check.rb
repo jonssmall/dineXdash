1f93b4562af379345f260298ad3916f665a81bc9
Jon <jonssmall@gmail.com>
2014-03-06 16:59:53 -0500

    Basic subtotal logic built into views.

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,10 +1,18 @@
 class Check < ActiveRecord::Base
-	has_many :check_items
+	has_many :check_items, dependent: :destroy
 	belongs_to :diner, class_name: 'User', foreign_key: :user_id
 	belongs_to :restaurant
 	# user_id, :restaurant_id, :paid_at
 	validates :user_id, :restaurant_id, :presence => true
 
 	scope :pending, -> { where(paid_at: nil) }
+
+	def total
+		total = 0
+		# check_items.each do |item|
+	 # 		total += item.subtotal
+		# end
+		# return total
+	end
 
 end
--- app/models/check_item.rb
+++ app/models/check_item.rb
@@ -3,4 +3,8 @@ class CheckItem < ActiveRecord::Base
 	validates :item_name, :item_desc, :price, :presence => true
 	# restaurant_id, :item_name, :item_desc, :price
 	validates :quantity, numericality: { only_integer: true, :greater_than_or_equal_to => 1 }
+
+	def subtotal
+		price * quantity
+	end
 end
