21b45ce9e04f9fbf169ea6a61e294e181fe382ab
Anika Tabassum <anika01@gmail.com>
2014-03-09 22:59:23 -0400

    Testing popup for login and accordion for displaying restaurants: see application.html.erb and users/_form.html.erb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 DineXdash::Application.initialize!
 
-config.assets.debug = true
+# config.assets.debug = true
