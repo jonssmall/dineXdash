7b1dacba11986dda3cb1cbcfe926757f87bde99b
Jon <jonssmall@gmail.com>
2014-03-12 19:18:05 -0400

    Fixed accordion rubber band bug. Still need to fix colors and size of active panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,11 +26,17 @@ $(function(){
 // $('#footertest').addClass('animated slideInLeft');
 
 // WORKING SOLUTION BUT TAB DOES NOT CLOSE
-$(".accordion dd").on("click", "a:eq(0)", function (event)
+       $(".accordion dd").on("click", "a:eq(0)", function (event)
       {
-      	var dd_parent= $(this).parent();
-        $(".accordion dd div.content:visible").slideToggle("normal");
-        $(this).parent().find(".content").slideToggle("normal");
+        var dd_parent = $(this).parent();
+
+        if(dd_parent.hasClass('active'))
+          $(".accordion dd div.content:visible").slideToggle("normal");
+        else
+        {
+          $(".accordion dd div.content:visible").slideToggle("normal");
+          $(this).parent().find(".content").slideToggle("normal");
+        }
       });
 
 
